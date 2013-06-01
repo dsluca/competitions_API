232f7935e095b2ff7d377f213273987bd509a2ea
Dylan Lucas <dylan.lucas@gmail.com>
2013-06-01 17:56:04 +0100

    moved code around a bit

--- status.py
+++ status.py
@@ -6,34 +6,20 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
+'''
+---- ROUTES ----
+'''
 @app.route('/')
 def home():
 	print "does this log?"
 	return jsonify(status="OK")
+
 
 @app.route('/ladder/get')
 def getLadder():
 	l = Ladder()
 	return jsonify(players=l.getPlayers())
 
-
-class Ladder:
-	"""Ladder """
-
-	def getPlayers(self):
-		dylan = PlayerRank(1,"dylan",1,0)
-		danny = PlayerRank(2,"danny",1,0)
-		gerik = PlayerRank(3,"gerik",1,0)
-		simon = PlayerRank(4,"simon",1,0)
-		return [dylan.__dict__, danny.__dict__, gerik.__dict__, simon.__dict__]
-
-class PlayerRank:
-	"""Represents a player and its points """
-	def __init__(self,playerId,username,rank, points):
-		self.playerId = playerId
-		self.username = username
-		self.rank = rank
-		self.points = points
 
 @app.route('/db/test')
 def dbTest():
@@ -54,6 +40,27 @@ def create_db(name, email):
 	db.session.commit()
 	status = {"status": "OK"}
 	return jsonify(result=status)
+
+'''
+---- Classes ----
+'''
+class Ladder:
+	"""Ladder """
+
+	def getPlayers(self):
+		dylan = PlayerRank(1,"dylan",1,0)
+		danny = PlayerRank(2,"danny",1,0)
+		gerik = PlayerRank(3,"gerik",1,0)
+		simon = PlayerRank(4,"simon",1,0)
+		return [dylan.__dict__, danny.__dict__, gerik.__dict__, simon.__dict__]
+
+class PlayerRank:
+	"""Represents a player and its points """
+	def __init__(self,playerId,username,rank, points):
+		self.playerId = playerId
+		self.username = username
+		self.rank = rank
+		self.points = points
 
 '''
 Model
