b81ab43d875e5b8405c9f600b6b8379eb1a9b217
Dylan Lucas <dylan.lucas@gmail.com>
2013-06-01 19:15:48 +0100

    some changes for pep8 compliance

--- status.py
+++ status.py
@@ -9,81 +9,88 @@ db = SQLAlchemy(app)
 '''
 ---- ROUTES ----
 '''
+
+
 @app.route('/')
 def home():
-	print "does this log?"
-	return jsonify(status="OK")
+    print "does this log?"
+    return jsonify(status="OK")
 
 
 @app.route('/ladder/get')
 def getLadder():
-	l = Ladder()
-	return jsonify(players=l.getPlayers())
+    l = Ladder()
+    return jsonify(players=l.getPlayers())
 
 
 @app.route('/db/test')
 def dbTest():
-	print "OK"
-	print "querying"
-	all_users = Player.query.all()
-	userx = all_users.pop()
-	print all_users
-	print userx
-	return jsonify(X=userx.email)
+    print "OK"
+    print "querying"
+    all_users = Player.query.all()
+    userx = all_users.pop()
+    print all_users
+    print userx
+    return jsonify(X=userx.email)
 
 
 @app.route('/db/create/player/<string:name>/<string:email>')
 def create_db(name, email):
-	print "creating " + email
-	player = Player(name, email)
-	db.session.add(player)
-	db.session.commit()
-	status = {"status": "OK"}
-	return jsonify(result=status)
+    print "creating " + email
+    player = Player(name, email)
+    db.session.add(player)
+    db.session.commit()
+    status = {"status": "OK"}
+    return jsonify(result=status)
 
 '''
 ---- Classes ----
 '''
-class Ladder:
-	"""Ladder """
 
-	def getPlayers(self):
-		dylan = PlayerRank(1,"dylan",1,0)
-		danny = PlayerRank(2,"danny",1,0)
-		gerik = PlayerRank(3,"gerik",1,0)
-		simon = PlayerRank(4,"simon",1,0)
-		return [dylan.__dict__, danny.__dict__, gerik.__dict__, simon.__dict__]
+
+class Ladder:
+    """Ladder """
+
+    def getPlayers(self):
+        dylan = PlayerRank(1, "dylan", 1, 0)
+        danny = PlayerRank(2, "danny", 1, 0)
+        gerik = PlayerRank(3, "gerik", 1, 0)
+        simon = PlayerRank(4, "simon", 1, 0)
+        return [dylan.__dict__, danny.__dict__, gerik.__dict__, simon.__dict__]
+
 
 class PlayerRank:
-	"""Represents a player and its points """
-	def __init__(self,playerId,username,rank, points):
-		self.playerId = playerId
-		self.username = username
-		self.rank = rank
-		self.points = points
+    """Represents a player and its points """
+    def __init__(self, playerId, username, rank, points):
+        self.playerId = playerId
+        self.username = username
+        self.rank = rank
+        self.points = points
 
 '''
 Model
 '''
+
+
 class Player(db.Model):
-	playerId = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(80))
-	email = db.Column(db.String(120), unique=True)
-	challenges = db.relationship('Challenge', backref='player', lazy='dynamic')
+    playerId = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    email = db.Column(db.String(120), unique=True)
+    challenges = db.relationship('Challenge', backref='player', lazy='dynamic')
 
-	def __init__(self, name, email):
-		self.name = name
-		self.email = email
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
 
-	def __repr__(self):
-		return '<Name %r>' % self.name
+    def __repr__(self):
+        return '<Name %r>' % self.name
 
 
 class Challenge(db.Model):
-	challengeId = db.Column(db.Integer, primary_key=True)
-	player1 = db.Column(db.Integer, db.ForeignKey('player.playerId'))
-	player2 = db.Column(db.Integer, db.ForeignKey('player.playerId'))
+    challengeId = db.Column(db.Integer, primary_key=True)
+    player1 = db.Column(db.Integer, db.ForeignKey('player.playerId'))
+    player2 = db.Column(db.Integer, db.ForeignKey('player.playerId'))
 
-	def __init__(self, player1, player2):
-		self.player1 = player1
-		self.player2 = player2
+    def __init__(self, player1, player2):
+        self.player1 = player1
+        self.player2 = player2
