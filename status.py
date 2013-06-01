f1e726351876e1b6d2038eff2ac6cfd4356b691c
Dylan Lucas <dylan.lucas@gmail.com>
2013-06-01 17:25:05 +0100

    can now communicate with the db. Have a simple model (player and challenge) and have a route to create new players

--- status.py
+++ status.py
@@ -1,16 +1,19 @@
 import os
 from flask import Flask, jsonify
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 
 @app.route('/')
 def home():
+	print "does this log?"
 	return jsonify(status="OK")
 
 @app.route('/ladder/get')
 def getLadder():
 	l = Ladder()
-	dylan = PlayerRank(1,"dylan",1,0)
 	return jsonify(players=l.getPlayers())
 
 
@@ -32,4 +35,48 @@ class PlayerRank:
 		self.rank = rank
 		self.points = points
 
-	
+@app.route('/db/test')
+def dbTest():
+	print "OK"
+	print "querying"
+	all_users = Player.query.all()
+	userx = all_users.pop()
+	print all_users
+	print userx
+	return jsonify(X=userx.email)
+
+
+@app.route('/db/create/player/<string:name>/<string:email>')
+def create_db(name, email):
+	print "creating " + email
+	player = Player(name, email)
+	db.session.add(player)
+	db.session.commit()
+	status = {"status": "OK"}
+	return jsonify(result=status)
+
+'''
+Model
+'''
+class Player(db.Model):
+	playerId = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(80))
+	email = db.Column(db.String(120), unique=True)
+	challenges = db.relationship('Challenge', backref='player', lazy='dynamic')
+
+	def __init__(self, name, email):
+		self.name = name
+		self.email = email
+
+	def __repr__(self):
+		return '<Name %r>' % self.name
+
+
+class Challenge(db.Model):
+	challengeId = db.Column(db.Integer, primary_key=True)
+	player1 = db.Column(db.Integer, db.ForeignKey('player.playerId'))
+	player2 = db.Column(db.Integer, db.ForeignKey('player.playerId'))
+
+	def __init__(self, player1, player2):
+		self.player1 = player1
+		self.player2 = player2
